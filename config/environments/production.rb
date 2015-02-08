7098a8fa77cf91d060099e5cc04d3f4e9ec6a9f6
pietrop <pietro.passarelli@gmail.com>
2015-02-08 10:43:40 +0000

    adding send grid for password reset for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.action_mailer.default_url_options = { :host => 'autoedit.heroku.com' }#
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'autoedit.heroku.com' }#
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+
 # for heroku
 # RAILS_ENV=production bundle exec rake assets: precompile
   # Code is not reloaded between requests.
