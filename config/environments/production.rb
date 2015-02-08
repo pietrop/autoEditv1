0153dc4c42bed016f6974691b0b9706091683c62
pietrop <pietro.passarelli@gmail.com>
2015-02-08 11:15:05 +0000

    added srt file upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,11 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'beta.autoedit.io' }#
+  config.action_mailer.default_url_options = { :host => 'autoedit.herokuapp.com' }#
 
 config.action_mailer.delivery_method = :smtp
 
-Rails.application.routes.default_url_options[:host] = 'beta.autoedit.io'
+Rails.application.routes.default_url_options[:host] = 'autoedit.herokuapp.com'
 
   config.action_mailer.perform_deliveries = true #
   config.action_mailer.raise_delivery_errors = false#
@@ -19,7 +19,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'beta.autoedit.io',
+  :domain         => 'autoedit.herokuapp.com',
   :enable_starttls_auto => true
 }
 
